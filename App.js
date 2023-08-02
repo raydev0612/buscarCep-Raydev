5f5e01acd42aea7b0cfdd8ba7cd9eb6ea5353f15
RayDev <raydev0612@gmail.com>
2023-08-01 22:15:22 -0300

    commit sin build

--- App.js
+++ App.js
@@ -1,20 +1,147 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Api from './src/services/api'
 
 export default function App() {
+  const [cep, setCep] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [localidade, setLocalidade] = useState("");
+  const [uf, setUf] = useState("");
+
+  const buscarCep = async () => {
+    if(cep == ''){
+      Alert.alert('Faltam Dados', 'El campo Cep não pode ficar vacío!')
+      setCep('')
+    }
+
+    try {
+      const response = await Api.get(`/${cep}/json/`)
+      setLogradouro(response.data.logradouro)
+      setBairro(response.data.bairro)
+      setLocalidade(response.data.localidade)
+      setUf(response.data.uf)
+    } catch (error) {
+      console.log("Error" + error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerPrincipal}>
+      <View style={styles.topBar}>
+        <Text style={styles.title}>Buscador de CEP</Text>
+      </View>
+      <View style={styles.containerCep}>
+        <TextInput
+          style={{
+            borderColor: "#888",
+            borderWidth: 2,
+            width: 200,
+            fontSize: 18,
+            marginTop: 30,
+            marginEnd: 20,
+            padding: 15,
+            borderRadius: 10,
+          }}
+          value={cep}
+          onChangeText={(texto) => setCep(texto)}
+          placeholder="Cep"
+        />
+        <TouchableOpacity style={styles.botaoBuscar} onPress={buscarCep}>
+          <Text style={styles.textoBotaoBuscar}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TextInput
+        style={styles.caixaTexto}
+        value={logradouro}
+        onChangeText={(texto) => setLogradouro(texto)}
+        placeholder="Rua ou Logradouro"
+      />
+      <TextInput
+        style={styles.caixaTexto}
+        value={bairro}
+        onChangeText={(texto) => setBairro(texto)}
+        placeholder="Bairro"
+      />
+      <TextInput
+        style={styles.caixaTexto}
+        value={localidade}
+        onChangeText={(texto) => setLocalidade(texto)}
+        placeholder="Cidade"
+      />
+      <TextInput
+          style={{
+            borderColor: "#888",
+            borderWidth: 2,
+            width: 100,
+            fontSize: 18,
+            marginTop: 10,
+            marginEnd: 20,
+            padding: 15,
+            borderRadius: 10,
+            marginHorizontal: 20
+          }}
+          value={uf}
+          onChangeText={(texto) => setUf(texto)}
+          placeholder="Estado"
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerPrincipal: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+  },
+  topBar: {
+    flexDirection: "row",
+    height: 70,
+    backgroundColor: "#018786",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf: "center",
+    margin: 20,
+  },
+  containerCep: {
+    flexDirection: "row",
+    height: 100,
+    marginHorizontal: 20,
+  },
+  botaoBuscar: {
+    backgroundColor: "#018786",
+    width: 120,
+    height: 70,
+    marginTop: 30,
+    marginEnd: 20,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textoBotaoBuscar: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  caixaTexto: {
+    borderColor: "#000",
+    borderWidth: 2,
+    padding: 15,
+    fontSize: 18,
+    borderRadius: 10,
+    marginTop: 10,
+    marginHorizontal: 20,
   },
 });
